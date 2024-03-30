a1388843fd354dc150a8bf45cf55a6746bf5650a
NITHYAPRIYA M <142155664+nithyapriya-din@users.noreply.github.com>
2024-03-30 16:49:17 +0530

    product.queries.js

---
+++ DATABASE_DATA/product.queries.js
@@ -0,0 +1,142 @@
+1.Find all information about each product:
+[
+    {
+        "id": "1",
+        "product_name": "Intelligent Fresh Chips",
+        "product_price": 655.00,
+        "product_material": "Concrete",
+        "product_color": "mint green"
+    },
+    {
+        "id": "2",
+        "product_name": "Practical Fresh Sausages",
+        "product_price": 911.0,
+        "product_material": "Cotton",
+        "product_color": "indigo"
+    },
+    {
+        "id": "3",
+        "product_name": "Refined Steel Car",
+        "product_price": 690.00,
+        "product_material": "Rubber",
+        "product_color": "gold"
+    },
+    ...
+]
+2.Find the product price which are between 400 to 800:
+[
+    {
+        "id": "1",
+        "product_name": "Intelligent Fresh Chips",
+        "product_price": 655.00,
+        "product_material": "Concrete",
+        "product_color": "mint green"
+    },
+    {
+        "id": "3",
+        "product_name": "Refined Steel Car",
+        "product_price": 690.00,
+        "product_material": "Rubber",
+        "product_color": "gold"
+    },
+    ...
+]
+3.Find the product price which are not between 400 to 600:
+[
+    {
+        "id": "2",
+        "product_name": "Practical Fresh Sausages",
+        "product_price": 911.0,
+        "product_material": "Cotton",
+        "product_color": "indigo"
+    },
+    ...
+]
+4.List the four products which are greater than 500 in price:
+[
+    {
+        "id": "1",
+        "product_name": "Intelligent Fresh Chips",
+        "product_price": 655.00,
+        "product_material": "Concrete",
+        "product_color": "mint green"
+    },
+    {
+        "id": "2",
+        "product_name": "Practical Fresh Sausages",
+        "product_price": 911.0,
+        "product_material": "Cotton",
+        "product_color": "indigo"
+    },
+    {
+        "id": "3",
+        "product_name": "Refined Steel Car",
+        "product_price": 690.00,
+        "product_material": "Rubber",
+      
+        "product_color": "gold"
+    },
+    {
+        "id": "4",
+        "product_name": "Gorgeous Plastic Pants",
+        "product_price": 492.00,
+        "product_material": "Soft",
+        "product_color": "plum"
+    }
+]
+5.Find the product name and product material of each product:
+[
+    {
+        "product_name": "Intelligent Fresh Chips",
+        "product_material": "Concrete"
+    },
+    {
+        "product_name": "Practical Fresh Sausages",
+        "product_material": "Cotton"
+    },
+    ...
+]
+
+6.Find the product with a row id of 10:
+{
+    "id": "10",
+    "product_name": "Generic Wooden Pizza",
+    "product_price": 84.00,
+    "product_material": "Frozen",
+    "product_color": "indigo"
+}
+7.Find all products which contain the value of 'soft' in product material:
+[
+    {
+        "id": "4",
+        "product_name": "Gorgeous Plastic Pants",
+        "product_price": 492.00,
+        "product_material": "Soft",
+        "product_color": "plum"
+    },
+    {
+        "id": "9",
+        "product_name": "Awesome Wooden Ball",
+        "product_price": 28.00,
+        "product_material": "Soft",
+        "product_color": "azure"
+    },
+    {
+        "id": "19",
+        "product_name": "Intelligent Cotton Chips",
+        "product_price": 46.00,
+        "product_material": "Soft",
+        "product_color": "azure"
+    }
+]
+8.Find products which contain product color 'indigo' and product price '492.00':
+[
+    {
+        "id": "4",
+        "product_name": "Gorgeous Plastic Pants",
+        "product_price": 492.00,
+        "product_material": "Soft",
+        "product_color": "plum"
+    }
+]
+
